e926935eed40d908df76e427d79a2efb306b0b05
Aryan <aryanprajapati9667@gmail.com>
2024-04-25 12:54:45 +0530

    Fix import error and update configuration in server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import { v2 } from 'cloudinary';
 import Razorpay from 'razorpay';
-
+import { config } from 'dotenv';
 import app from './app.js';
 import connectToDB from './configs/dbConn.js';
+
+config();
 
 // Cloudinary configuration
 v2.config({
@@ -13,7 +15,7 @@ v2.config({
 
 // Razorpay configuration
 export const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
+  key_id: process.env.RAZORPAY_KEY_ID|| rzp_test_IKBIG5hmA1fSbO,
   key_secret: process.env.RAZORPAY_SECRET,
 });
 
