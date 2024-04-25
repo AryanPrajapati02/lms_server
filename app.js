6481866a2b3d768801e0c2e2d56fc709d793c322
Aryan <aryanprajapati9667@gmail.com>
2024-04-25 13:18:13 +0530

    Update user route in app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ import courseRoutes from './routes/course.routes.js';
 import paymentRoutes from './routes/payment.routes.js';
 import miscRoutes from './routes/miscellaneous.routes.js';
 
-app.use('/api/v1/user', userRoutes);
+app.use('/', userRoutes);
 app.use('/api/v1/courses', courseRoutes);
 app.use('/api/v1/payments', paymentRoutes);
 app.use('/api/v1', miscRoutes);
